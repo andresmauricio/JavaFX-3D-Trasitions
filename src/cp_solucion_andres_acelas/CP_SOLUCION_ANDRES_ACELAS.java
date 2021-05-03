050bd50d3451256eb7bb3ef045db7a1754d7780b
andresmauricio <andresacelas@outlook.com>
2021-05-03 12:45:17 -0600

    init project

---
+++ src/cp_solucion_andres_acelas/CP_SOLUCION_ANDRES_ACELAS.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cp_solucion_andres_acelas;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author PC
+ */
+public class CP_SOLUCION_ANDRES_ACELAS extends Application {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
